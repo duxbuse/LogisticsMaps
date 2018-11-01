e73265eb0b7c97e4497bb4b3f39ccfa50d1fad6a
Duxbury, Sean <duxburys@corptst.anz.com>
2018-11-01 17:09:21 +1100

    Updated Readme

--- Step1.google-container-cluster.terraform/Step1.google-container-cluster.tf
+++ Step1.google-container-cluster.terraform/Step1.google-container-cluster.tf
@@ -1,6 +1,7 @@
 resource "google_container_cluster" "primary" {
   name                     = "diceroller-cluster"
   remove_default_node_pool = true
+  initial_node_count = 1
 
   master_auth {
     username = "${var.username}"
@@ -11,7 +12,7 @@ resource "google_container_cluster" "primary" {
 resource "google_container_node_pool" "primary_pool" {
   name       = "primary-pool"
   cluster    = "${google_container_cluster.primary.name}"
-  node_count = "5"
+  node_count = "2"
 
   node_config {
     machine_type = "f1-micro"
